d1bdb6fa8151c218b72f92352eb0317a1d969e57
Diego Garrido Romero <diegogarridoromero@gmail.com>
2026-09-21 01:46:45 +0200

    Practica 0 Completada

--- app/features/auth/repositories.py
+++ app/features/auth/repositories.py
@@ -3,6 +3,25 @@ from splent_framework.repositories.BaseRepository import BaseRepository
 from app.features.auth.models import User
 
 
+
+from app.features.notepad.models import Notepad
+from splent_framework.repositories.BaseRepository import BaseRepository
+
+
+class NotepadRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(Notepad)
+
+    def get_all_by_user(self, user_id):
+        return self.get_by_column('user_id', user_id)
+
+
+
+
+
+
+
+
 class UserRepository(BaseRepository):
     def __init__(self):
         super().__init__(User)
